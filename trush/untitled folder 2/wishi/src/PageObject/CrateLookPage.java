7438e3f4973a7d156a8beb16c2a92bd8b995d454
luzifere <yinonwishi@yinons-MacBook-Pro.local>
2018-01-14 18:15:57 +0200

    return wishi/wishimobile

---
+++ trush/untitled folder 2/wishi/src/PageObject/CrateLookPage.java
@@ -0,0 +1,103 @@
+package PageObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+
+public class CrateLookPage extends BasePage {
+
+	public CrateLookPage(WebDriver driver) {
+		super(driver);
+		// TODO Auto-generated constructor stub
+	}
+
+	@FindBy(xpath = "(//div//button[@class='create-look-btn ng-binding ng-scope'])[1]")
+	WebElement CrateLookButton;
+	@FindBy(xpath = "//div//button[@class='create-look-btn ng-binding']")
+	WebElement CrateLookButtonOfChat;
+
+	@FindBy(xpath = "//div//li//a[text()[contains(.,'Shop')]]")
+	WebElement ShopButton;
+	@FindBy(xpath = "//div//canvas[@class='upper-canvas ']")
+	WebElement Canvas;
+	@FindBy(xpath = "//div[@class='saveBtn']")
+	WebElement SaveButton;
+	@FindBy(xpath = "//div//textarea[@id='TextArea']")
+	WebElement Description;                //autoresize ng-pristine ng-valid ng-empty ng-valid-maxlength ng-touched
+	@FindBy(xpath = "//div//textarea[@placeholder='Tell your client why you chose those items']")
+	WebElement DescriptionOfPersonalShopping;
+	@FindBy(xpath = "	//button[@class='save ng-binding']")
+	WebElement SaveOfDescription;
+	@FindBy(css = ".ng-isolate-scope.fixed-height")
+	WebElement PersonalShoppingTab;
+	@FindBy(css = "//div//span[text()[contains(.,'My Bookings')]]")
+	WebElement MyBookingTab;
+
+	public void DragDropItems(String item) {
+		WebElement FromItem = driver.findElement(By.xpath(("(//div[@class='webSingleItemDisplayContainer drag-object ng-scope'])" + item)));
+		WebElement ToCanvas = Canvas;
+
+		Actions builder = new Actions(driver);
+		builder.click(FromItem);
+		builder.dragAndDrop(FromItem, ToCanvas).perform();
+
+	}
+
+	public void SelectItem(String item) {
+		BasePage.ExplicityWaitIsClickable(By.xpath("(//div[@class='webSingleItemDisplayContainer drag-object ng-scope'])" + item ));
+		Sleep(200);
+	}
+
+	public void SelectLayoutTabs(String tab) {
+		WebElement Layout = driver.findElement(By.xpath(("(//div[@id='layouts']//label)" + tab)));
+		click(Layout);
+	}
+
+	public void CrateLook() {
+
+		BasePage.ExplicityWaitIsClickable(By.xpath("(//div//button[@class='create-look-btn ng-binding ng-scope'])[1]"));
+	}
+	public void ClickShop() {
+
+		BasePage.ExplicityWaitIsClickable(By.xpath("//div//li//a[text()[contains(.,'Shop')]]"));
+	}
+	
+	public void SelectClass(String NameOfMenu) {
+		String MainMenuClass = ("//div//li//a[text()[contains(.,'Shop')]]");
+		MainMenuClass = MainMenuClass.replace("Shop", NameOfMenu);
+		WebElement MainMenu = driver.findElement(By.xpath(MainMenuClass));
+		click(MainMenu);
+	}
+
+	public void ClickSave() 
+	{
+		Sleep(200);
+		click(SaveButton);
+	}
+
+	public void FillDescription(String descriptiontext) {
+		ExplicityWaitIsClickable(By.xpath("//button[@class='save ng-binding']"));
+		filltext(Description, descriptiontext);
+	}
+
+	public void FillDescriptionPS(String descriptiontext) {
+		filltext(DescriptionOfPersonalShopping, descriptiontext);
+	}
+
+	public void ClickDescriptionPS() {
+		click(DescriptionOfPersonalShopping);
+	}
+
+	public void ClickNewLook() {
+		BasePage.ExplicityWaitIsClickable(By.xpath("//div//button[@class='create-look-btn ng-binding']"));
+	}
+
+	public void ClickSaveOfDescription() {
+		ExplicityWaitIsClickable(By.xpath("//button[@class='save ng-binding']"));
+		click(SaveOfDescription);
+	}
+
+}
---
+++ trush/untitled folder 2/workspace/WishiMobileWeb/src/PageObejecs/LoginPages.java
@@ -0,0 +1,35 @@
+package PageObejecs;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+public class LoginPages extends BasePage
+{
+	@FindBy(xpath="//div//p[@class='header-link-text']")
+	public WebElement LoginButton;
+	@FindBy(xpath="//div//input[@id='signInEmail']")
+	public WebElement UserName;
+	@FindBy(xpath="//div//input[@id='signInPass']")
+	public WebElement Password;
+	@FindBy(xpath="//div//input[@value='Log in']")
+	public WebElement LoginOfWizard;
+	
+	public LoginPages(AndroidDriver<AndroidElement> driver) {
+		super(driver);
+	}
+		
+	public void ClickLogin()
+	{
+	click(LoginButton);
+	}
+	public void DoLogin()
+	{
+		filltext(UserName, "yinon@wishi.me");
+		filltext(Password, "156523784");
+		click(LoginOfWizard);
+	}
+}
